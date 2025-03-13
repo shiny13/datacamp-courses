741d9edd4a159a6ba3278623b0286af09e042d05
Shahnawaz Alam <shahnawaz.alam13@gmail.com>
2025-03-13 12:50:24 +1100

    intro to python

---
+++ intro-to-python/python-basics.py
@@ -0,0 +1,8 @@
+print(1 + 2)
+print('1' + '2')
+
+height = 1.68
+weight = 70
+bmi = weight / height ** 2 # ** is used for squared 
+print(bmi)
+print(type(bmi))
---
+++ intro-to-python/readme.md
@@ -0,0 +1,3 @@
+# Introduction to Python
+
+This directory contains the basic concepts to learn python. Python 3 was used to learn and code in this repository.
