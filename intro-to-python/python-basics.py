0cc0c8b59fa28ec45ed7e73e3a32f3ccb648d300
Shahnawaz Alam <shahnawaz.alam13@gmail.com>
2025-03-13 13:53:14 +1100

    Intro to python. Basics

--- intro-to-python/python-basics.py
+++ intro-to-python/python-basics.py
@@ -1,8 +1,24 @@
 print(1 + 2)
 print('1' + '2')
 
+check = True
+print(check)
+print(type(check))
+print('check: ' + str(check))
+
 height = 1.68
 weight = 70
 bmi = weight / height ** 2 # ** is used for squared 
 print(bmi)
 print(type(bmi))
+
+monthly_savings = 10
+num_months = 4
+new_savings = monthly_savings * num_months
+print(new_savings)
+
+savings = 100
+new_savings = 40
+total_savings = savings + new_savings
+print(total_savings)
+print(type(total_savings))
